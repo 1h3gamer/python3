e8645d9f5db68c58e3a7957646dc16cb0db1f749
1h3gamer <Idaune.nathan@gmail.com>
2025-08-30 18:50:49 +0100

    project of python 2

---
+++ main3.py
@@ -0,0 +1,9 @@
+#storing birthdays in variables
+bir1="1/12/2013"
+bir2="2/8/2000"
+bir3="19/7/2016"
+bir4="24/2/2024"
+bir5="5/9/2019"
+
+#printing a sentence using variables
+print("Birthday 1 is: ",bir1,"birthday 2 is the: ",bir2,"birthday 3 is on: ",bir3,".Birthday 4 is the: ",bir4,".And lastly birthday 5 is on the",bir5)
